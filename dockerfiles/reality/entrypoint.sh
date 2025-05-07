eabd78b8c916cb4b8937e462dae73db470797172
wrlin1218 <wrlin1218@gmail.com>
2025-05-08 01:45:20 +0800

    feat: add reality docker file and example

---
+++ dockerfiles/reality/entrypoint.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+if [ -f /config/config_info.txt ]; then
+  echo "config_info.txt exist"
+else
+  IPV6=$(curl -6 -sSL --connect-timeout 3 --retry 2  ip.sb || echo "null")
+  IPV4=$(curl -4 -sSL --connect-timeout 3 --retry 2  ip.sb || echo "null")
+  
+  # 自动生成UUID
+  UUID="$(/xray uuid)"
+  echo "UUID: $UUID"
+
+  # 设置默认端口
+  EXTERNAL_PORT=443
+
+  # 设置DEST默认值
+  if [ -z "$DEST" ]; then
+    echo "DEST is not set. default value www.apple.com:443"
+    DEST="www.apple.com:443"
+  fi
+
+  # 设置SERVERNAMES默认值
+  if [ -z "$SERVERNAMES" ]; then
+    echo "SERVERNAMES is not set. use default value [\"www.apple.com\",\"images.apple.com\"]"
+    SERVERNAMES="www.apple.com images.apple.com"
+  fi
+
+  # 自动生成密钥对
+  echo "Generating new key pair"
+  /xray x25519 >/key
+  PRIVATEKEY=$(cat /key | grep "Private" | awk -F ': ' '{print $2}')
+  PUBLICKEY=$(cat /key | grep "Public" | awk -F ': ' '{print $2}')
+  echo "Private key: $PRIVATEKEY"
+  echo "Public key: $PUBLICKEY"
+
+  # 设置默认网络类型
+  NETWORK="tcp"
+
+  # 修改配置
+  jq ".inbounds[1].settings.clients[0].id=\"$UUID\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].streamSettings.realitySettings.dest=\"$DEST\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+
+  SERVERNAMES_JSON_ARRAY="$(echo "[$(echo $SERVERNAMES | awk '{for(i=1;i<=NF;i++) printf "\"%s\",", $i}' | sed 's/,$//')]")"
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[1].streamSettings.realitySettings.serverNames = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.routing.rules[0].domain = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+
+  jq ".inbounds[1].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].streamSettings.network=\"$NETWORK\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+
+  FIRST_SERVERNAME=$(echo $SERVERNAMES | awk '{print $1}')
+  # 生成配置信息
+  echo -e "\033[32m" >/config/config_info.txt
+  echo "IPV6: $IPV6" >>/config/config_info.txt
+  echo "IPV4: $IPV4" >>/config/config_info.txt
+  echo "UUID: $UUID" >>/config/config_info.txt
+  echo "DEST: $DEST" >>/config/config_info.txt
+  echo "PORT: $EXTERNAL_PORT" >>/config/config_info.txt
+  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config/config_info.txt
+  echo "PRIVATEKEY: $PRIVATEKEY" >>/config/config_info.txt
+  echo "PUBLICKEY: $PUBLICKEY" >>/config/config_info.txt
+  echo "NETWORK: $NETWORK" >>/config/config_info.txt
+  if [ "$IPV4" != "null" ]; then
+    SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#${IPV4}-Reality"
+    echo "IPV4 订阅连接: $SUB_IPV4" >>/config/config_info.txt
+    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/config/config_info.txt
+  fi
+  if [ "$IPV6" != "null" ];then
+    SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#${IPV6}-Reality"
+    echo "IPV6 订阅连接: $SUB_IPV6" >>/config/config_info.txt
+    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/config/config_info.txt
+  fi
+
+  echo -e "\033[0m" >>/config/config_info.txt
+fi
+
+# 显示配置信息
+cat /config/config_info.txt
+
+# 运行xray
+exec /xray -config /config.json
